f71ed8975e9ed75e926c9072126171b493bd91a6
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-04-13 18:52:48 +0100

    removed vackend folder

--- backend/src/main/java/com/falconeye/backend/models/AssetType.java
+++
@@ -1,8 +0,0 @@
-package com.falconeye.backend.models;
-
-public enum AssetType {
-    NAVAL_VESSEL,
-    UAV_DRONE,
-    INFANTRY_UNIT,
-    AIR_BASE
-}
--- backend/src/main/java/com/falconeye/backend/models/Role.java
+++
@@ -1,7 +0,0 @@
-package com.falconeye.backend.models;
-
-public enum Role {
-    ADMIN, // Maps to COMMANDER
-    USER,  // Maps to ANALYST
-    FIELD_AGENT
-}
