839cda45f65f2f1716baf09bc2a19a52418986db
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-04-14 22:51:55 +0100

    fresh clean setup using a ready to go template

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    svgr({
+      exportAsDefault: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  server: {
+    host: true,
+    port: 5000,
+  },
 })
