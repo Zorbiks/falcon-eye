258a422d1c572d836207c667013491659eac650e
Zorbiks <176812563+Zorbiks@users.noreply.github.com>
2026-04-29 14:20:25 +0100

    Replace mock feed with actual feed

--- client/src/services/feedService.ts
+++ client/src/services/feedService.ts
@@ -1,17 +1,16 @@
 import axios from 'src/lib/axios'
 import type { FeedItem } from '../types/feed'
-import mockFeedResponse from 'src/data/feed-mock.json'
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080'
 
-export const fetchNewsFeed = async (): Promise<FeedItem[]> => {
+export const fetchNewsFeed = async (): Promise<FeedItem[] | null> => {
   try {
-    await axios.get(`/news/feed`)
+   const response =  await axios.get(`/news/feed`)
+   return response.data
   } catch (error) {
     console.error('Error fetching news feed from API. Returning mock data:', error)
+    return null
   }
-
-  return mockFeedResponse
 }
 
 export default fetchNewsFeed
