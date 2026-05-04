e18caa1ba66e9fe93a71dc65a526f85305e9f9ae
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-05-04 21:18:29 +0100

    fixed a typo in the data pipeline

--- client/src/services/feedService.ts
+++ client/src/services/feedService.ts
@@ -1,14 +1,12 @@
-import axios from 'src/lib/eventsAPI'
+import api from 'src/lib/newsAPI'
 import type { FeedItem } from '../types/feed'
-
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8081'
 
 export const fetchNewsFeed = async (): Promise<FeedItem[] | []> => {
   try {
-    const response = await axios.get(`/news/feed`)
+    const response = await api.get(`/feed`)
     return response.data
   } catch (error) {
-    console.error('Error fetching news feed from API. Returning mock data:', error)
+    console.error('Error fetching news feed from API:', error)
   }
   return []
 }
